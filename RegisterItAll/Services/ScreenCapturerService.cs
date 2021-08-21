10c83ad6066f4599fe9e561f81976b258d2ced12
Alejandro <alejandrocarrion1999@gmail.com>
2021-08-21 15:45:25 +0200

    Implementar ScreenCapturerService.

--- RegisterItAll/Services/ScreenCapturerService.cs
+++ RegisterItAll/Services/ScreenCapturerService.cs
@@ -1,19 +1,37 @@
 ﻿using RegisterItAll.Services.Base;
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RegisterItAll.Services
 {
     public partial class ScreenCapturerService : ExecutableAsConsoleApplicationService
     {
-        protected override void OnStart(string[] args)
+        private const int DelaySecons = 5;
+
+        protected override async void OnStart(string[] args)
         {
             while (true)
             {
-                Console.WriteLine("-");
+                TakeScreenshot();
 
-                Task.Delay(50);
+                await Task.Delay(DelaySecons * 1000);
             }
+        }
+
+        private void TakeScreenshot()
+        {
+            Rectangle screen = Screen.GetBounds(new Point(0, 0));
+
+            Bitmap bitmap = new Bitmap(screen.Width, screen.Height);
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            string screenshotName = $"screenshot_{DateTime.Now.ToString().Replace('/', '.').Replace(' ', '_').Replace(':', '.')}.jpeg";
+
+            graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+            bitmap.Save(screenshotName, ImageFormat.Jpeg);
         }
     }
 }
