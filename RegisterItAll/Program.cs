5b070254ad8fd9257cde32150001e6d249125e20
Alejandro <alejandrocarrion1999@gmail.com>
2021-08-23 12:48:26 +0200

    Esconder la consola a menos que se pase un parámetro.

--- RegisterItAll/Program.cs
+++ RegisterItAll/Program.cs
@@ -1,12 +1,23 @@
 ﻿using RegisterItAll.Services;
 using RegisterItAll.Services.Base;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace RegisterItAll
 {
     public static class Program
     {
+        [DllImport("user32.dll")]
+        private extern static int ShowWindow(System.IntPtr hWnd, int nCmdShow);
+
         public static void Main(string[] args)
         {
+            if (!args.ToList().Contains("-ShowConsole"))
+            {
+                ShowWindow(Process.GetCurrentProcess().MainWindowHandle, 0);
+            }
+
             Service[] servicesToRun = new Service[]
             {
                 new EmailSenderService(),
